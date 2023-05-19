044e898b32cd3ec5473e08a6f1abce973a63f781
Tomasz Radtke <tomasz.radtke@oponeo.pl>
2023-05-19 13:31:03 +0200

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+public class MyStopwatch
+{
+    private DateTimeOffset _start;
+    private DateTimeOffset _stop;
+    private TimeSpan _interval;
+    private bool _isRunning = false;
+
+    public void Start()
+    {
+        if (_isRunning)
+        {
+            return;
+        }
+        _start = DateTimeOffset.Now;
+        _isRunning = true;
+    }
+    public void Stop()
+    {
+        if (_isRunning)
+        {
+            _stop = DateTimeOffset.Now;
+        }
+    }
+    public TimeSpan GetInterval()
+    {
+        if (_interval == default)
+        {
+            throw new InvalidOperationException("Stop the stopwatch to measure the time first.");
+        }
+        else
+        {
+            return _interval;
+        }
+    }
+}
